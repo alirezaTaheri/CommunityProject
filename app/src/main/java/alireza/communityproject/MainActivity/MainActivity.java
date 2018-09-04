32a5b47088c0161e0ebd840bcd99bdb82606ab50
Alireza Taheri <alireza.taheri210@gmail.com>
2018-09-04 09:14:20 +0430

    Some Packages Added

--- app/src/main/java/alireza/communityproject/MainActivity/MainActivity.java
+++ app/src/main/java/alireza/communityproject/MainActivity/MainActivity.java
@@ -1,7 +1,9 @@
-package alireza.communityproject;
+package alireza.communityproject.MainActivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import alireza.communityproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/alireza/communityproject/Profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package alireza.communityproject.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alireza.communityproject.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
